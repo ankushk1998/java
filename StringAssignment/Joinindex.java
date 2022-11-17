bd666bc69cc3ae2c204113d9ea5eff85886b5758
ankushk1998 <105196915+ankushk1998@users.noreply.github.com>
2022-11-17 17:34:39 +0530

    Add files via upload

---
+++ StringAssignment/Joinindex.java
@@ -0,0 +1,44 @@
+package com.String_Assignment;
+
+public class Join_at_index {
+
+	public static String A(String s,String n, int ind){
+	      
+	  
+	        
+	        String Str = new String(); 
+	  
+	        for (int i = 0; i < s.length(); i++) { 
+	  
+	            
+	            Str += s.charAt(i); 
+	  
+	            if (i == ind) {
+	                Str += n; 
+	            } 
+	        } 
+	  
+	       
+	        return Str; 
+	    } 
+	  
+	  
+	    public static void main(String[] args) 
+	    { 
+	  
+	      
+	        String a= "ankush"; 
+	        String b = "aa"; 
+	        int d = 4; 
+	   
+	        System.out.println("index: "
+                    + d); 
+	        System.out.println("new String: "
+	                           + a); 
+	        System.out.println("after join String: "
+	                           + A(a, b, d)); 
+	    } 
+
+	}
+
+
